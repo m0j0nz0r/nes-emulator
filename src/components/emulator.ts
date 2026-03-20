1e3cf324cb92ad110760ba776628bb4bd50405fe
Victor Hernandez <erladillao@gmail.com>
2026-03-20 12:18:57 -0500

    Add default palette data loading in Emulator constructor

--- src/components/emulator.ts
+++ src/components/emulator.ts
@@ -1,4 +1,6 @@
 import * as NanoTimer from 'nanotimer';
+import * as fs from 'fs';
+import * as path from 'path';
 import { Bus } from './bus';
 import { RAM } from './RAM';
 import { Nes6502 } from './nes6502';
@@ -6,8 +8,10 @@ import { Cartridge } from './cartridge';
 import { PPU } from './ppu';
 import { EventHandler, Logger } from './eventHandler';
 
+const defaultPaletteData = fs.readFileSync(path.join(__dirname, '../palettes/2C02G_wiki.pal'));
+
 export class Emulator extends EventHandler {
-    constructor (paletteData: Buffer, logger?: Logger) {
+    constructor (paletteData: Buffer = defaultPaletteData, logger?: Logger) {
         super(logger);
         this.bus = new Bus();
         this._graphicBus = new Bus();
